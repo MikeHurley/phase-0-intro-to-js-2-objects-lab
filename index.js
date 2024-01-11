e649d10e08f270661f7fda9bd96cd27f62977431
MikeHurley <154265547+MikeHurley@users.noreply.github.com>
2024-01-11 13:14:47 -0500

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+// Write your solution in this file
+const employee = {
+    name: "Sam",
+    streetAddress: "12 Broadway"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const updatedEmployee = {...employee};
+    updatedEmployee[key] = value
+    return updatedEmployee
+}
+updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway');
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee, 'streetAddress', '12 Broadway');
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newObj = {...employee};
+    delete newObj[key];
+    return newObj;
+}
+let newEmployee = deleteFromEmployeeByKey(employee, "name");
+
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+let newEmployees = destructivelyDeleteFromEmployeeByKey(employee, 'name');
